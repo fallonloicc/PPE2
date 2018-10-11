03781e9c41459621b6a9c1440f7caab947ebc5de
Loïc Fallon <thebose@live.fr>
2018-10-11 14:53:56 +0200

    Bdd + Insert

--- func/insert.php
+++ func/insert.php
@@ -1,3 +1,19 @@
 <?php
+  include("db.php");
+
+  if (isset($_POST['prix'], $_POST['libelle'], $_POST['mac'], $_POST['ip']))
+  {
+    $prix = $_POST['prix'];
+    $libelle = $_POST['libelle'];
+    $mac = $_POST['mac'];
+    $ip = $_POST['ip'];
+    $requete = 'INSERT INTO `bornes` ( `prix`, `libelle`, `adresseMac`, `adresseIP`) VALUES ( "'.$prix.'" , "'.libelle.'", "'.$mac.'", "'.$ip.'")';
+  }
 
 ?>
+<h1>Ajouter Bornes</h1>
+<input type="text" placeholder="Prix" name="prix">
+<input type="text" placeholder="Libelle" name="libelle">
+<input type="text" placeholder="Adresse Mac" name="mac">
+<input type="text" placeholder="Adresse IP" name="ip">
+<button type="submit" class="btn btn-primary" id="sendMessageButton">Envoyez</button>
