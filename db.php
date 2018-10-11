1292f8af68a40a01610526d8e8c421cd9b6eed5d
Loïc Fallon <thebose@live.fr>
2018-10-11 14:05:07 +0200

    Lien bdd

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+  try
+  {
+    $bdd = new PDO('mysql:host=localhost;dbname=tp2', "root", "");
+    $bdd->setAttribute (PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    //echo "connexion validée";
+  }
+  catch(PDOException $e)
+  {
+    echo "impossible de se connecter";
+    die();
+  }
+?>
