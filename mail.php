abc0e6a68cbbc12fd8abad1265a8b5d5b043c08c
gregoire <gregoiresomon@gmail.com>
2018-10-12 09:46:22 +0200

    update mail
    ajout de l'envoi de mail

--- mail.php
+++ mail.php
@@ -14,11 +14,40 @@
 	
 	$decode = json_decode(file_get_contents($api_url), true);
 	
-	if ($decode['success'] == true) {
+	if ($decode['success'] == true) 
+	{
 		// C'est un humain, mettre le code pour le mail
+		//vérifier si les champs sont vide 
+		if(empty($_POST['name'])  	||
+		   empty($_POST['email']) 	||
+		   empty($_POST['subject']) 	||
+		   empty($_POST['message'])	||
+		   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+		   {
+			echo "No arguments Provided!";
+			return false;
+		   }
+			
+		$name = $_POST['name'];
+		$email_address = $_POST['email'];
+		$subject = $_POST['subject'];
+		$message = $_POST['message'];
+			
+		// creer  un email et l'envoi	
+		$to = 'gregoiresomon@gmail.com'; // mettre votre adresse email ici
+		$email_subject = $subject;
+		$email_body = "You have received a new message. \n\n".
+						  " Here are the details:\n \nName: $name \n ".
+						  "Email: $email_address\n Message: \n $message";
+		$headers = "From: noreply@yourdomain.com\n"; 
+		// Since this email form will be generated from your server. The From email address will be best using something like this 		noreply@yourdomain.com
+		$headers .= "Reply-To: $email_address";	
+		mail($to,$email_subject,$email_body,$headers);
+		return true;			
 	}
 	
-	else {
+	else 
+	{
 		// C'est un robot ou le code de vérification est incorrecte, mettre une erreur
 	}
 		
